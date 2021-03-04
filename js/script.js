6e6ddbbcc6584ce3fcc107a50f081e5f2e99cd11
lianarego <78754476+lianarego@users.noreply.github.com>
2021-03-04 00:09:25 -0500

    Add code for cancel animation of menu button

--- js/script.js
+++ js/script.js
@@ -7,4 +7,13 @@ function menuToggle() {
   } else {
     x.className = 'navtoggle';
   }
+
+  var y = document.getElementById('menu-icon');
+  if (y.className === 'fas fa-bars') {
+    y.className = 'fas fa-times';
+    $('.navtoggle li .icon').css("font-size" , "50px");
+  } else {
+    y.className = 'fas fa-bars';
+    $('.navtoggle li .icon').css("font-size" , "40px");
+  }
 }
