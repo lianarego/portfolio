ffda61a969cb20fab57899f01ff0856c2aac6da9
lianarego <78754476+lianarego@users.noreply.github.com>
2021-04-08 16:27:43 -0400

    CSS linter code

--- js/script.js
+++ js/script.js
@@ -9,17 +9,6 @@ function menuToggle() {
     myNavToggle.className = 'navtoggle';
   }
 
-  /*
-  var menuIcon = document.getElementById('menu-icon');
-  if (menuIcon.className === 'fas fa-bars') {
-    menuIcon.className = 'fas fa-times';
-    $('.navtoggle li .icon').css("font-size" , "50px");
-  } else {
-    menuIcon.className = 'fas fa-bars';
-    $('.navtoggle li .icon').css("font-size" , "40px");
-  }
-  */
-
   var indexOverlay = document.getElementById('index-overlay');
   if (indexOverlay !== null) {
     if (indexOverlay.style.display === 'none' || indexOverlay.style.display === '') {
@@ -95,66 +84,6 @@ function menuClose() {
     projectHeader.style.backgroundColor = ''; // Transparent by default, but allow override by CSS media query
   }
 }
-
-/*
-function activateNiceScroll() {
-  // Console log
-  console.log('activateNiceScroll(): Window width =', window.innerWidth);
-
-  if (window.innerWidth > 414) {
-    // Console log
-    console.log('  niceScroll(): Window width =', window.innerWidth);
-
-    $("#grid-container").niceScroll({
-    cursorcolor: "rgba(255,130, 68,0.9)",
-    cursorwidth: "8px",
-    cursorborder: "0"})
-  } else {
-    // Console log
-    console.log('  getNiceScroll().remove(): Window width =', window.innerWidth);
-
-    $("#grid-container").getNiceScroll().remove();
-  }
-}
-activateNiceScroll();
-
-// Callback function when window is resized
-// window.onresize = function() { activateNiceScroll(); resetAnimation(); };
-window.onresize = function() { activateNiceScroll(); };
-
-// Disable horizontal scrolling in grid-container when niceScroll is enabled
-$("#grid-container").niceScroll({})
-*/
-
-
-/*
-// Automatically scroll to Projects section of the home page when user clicks on
-// the "Projects" nav link
-function scrollToProjects() {
-  gridContainer = document.getElementById('grid-container');
-
-  initialScrollLeft = gridContainer.scrollLeft;
-
-  var x = 0; // Fraction of displacement from old to new scroll position
-  updatePosition();
-  const id = setInterval(updatePosition, 10); // Total duration: 1000 milliseconds
-
-  function updatePosition() {
-    console.log('scrollToProjects(): updatePosition(): x =', x+',',
-      'scrollLeft =', gridContainer.scrollLeft);
-    if (x >= 1) {
-      clearInterval(id);
-      return null;
-    }
-    // gridContainer.scrollLeft = (x/100)*window.innerWidth +
-      // (1 - x/100)*initialScrollLeft;
-    var w = x**3 * (6*x**2 - 15*x + 10); // Fraction of smoothed displacement (Beta(3,3) CDF)
-    gridContainer.scrollLeft = w*window.innerWidth + (1-w)*initialScrollLeft;
-    x = x + 0.01;
-  }
-
-}
-*/
 
 // Scroll smoothly to anchor hyperlinks
 $(document).on('click', 'a[href^="#"]', function (event) {
